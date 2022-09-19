f388626ecb5d16205ff06b6e2e5e0c9f0a6354db
AlanQuaid <aljoshin.anton@gmail.com>
2022-09-19 22:22:54 +0300

    python version specified

--- timekeep_v1_1/timekeep_v1_1/settings.py
+++ timekeep_v1_1/timekeep_v1_1/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
 
 MEDIA_URL = '/media/'
